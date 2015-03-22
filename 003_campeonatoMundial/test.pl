bcf6b38257f4b4ce4b32e71daea5807d4b7f5e5c
Rodolfo Caputo <rodcap.93@gmail.com>
2015-03-22 02:45:28 -0300

    Arreglo test ej 3

--- 003_campeonatoMundial/test.pl
+++ 003_campeonatoMundial/test.pl
@@ -1,15 +1,15 @@
 test(test_rivales_argentina):-
 	findall(Rival,rival(argentina,Rival),Rivales),
-	memeber(nigeria,Rivales),
-	memeber(japon,Rivales),
-	memeber(escocia,Rivales).
+	member(nigeria,Rivales),
+	member(japon,Rivales),
+	member(escocia,Rivales).
 
 
 test(test_rivales_italia):-
 	findall(Rival,rival(italia,Rival),Rivales),
-	memeber(jamaica,Rivales),
-	memeber(colombia,Rivales),
-	memeber(camerun,Rivales).
+	member(jamaica,Rivales),
+	member(colombia,Rivales),
+	member(camerun,Rivales).
 
 test(test_japon_no_es_rival_de_japon):-
 not(rival(japon,japon)).
