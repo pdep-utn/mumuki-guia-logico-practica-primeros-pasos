8a7be44b9544cb989ffe9bd4edb0ba6021ecb252
Rodolfo Caputo <rodcap.93@gmail.com>
2015-03-22 02:20:00 -0300

    Arreglo los tests, dejando lo que se agregó en el merge

--- 001_empleadosCandidatos/test.pl
+++ 001_empleadosCandidatos/test.pl
@@ -1,3 +1,85 @@
+puedeAndar(comercioExterior,Persona):- habla(ingles,Persona),
+habla(frances,Persona),
+profesional(Persona).
+
+puedeAndar(comercioExterior,Persona):- ambicioso(Persona).
+puedeAndar(contaduria,Persona):- contador(Persona),
+honesto(Persona).
+
+puedeAndar(ventas,Persona):- ambicioso(Persona),
+conExperiencia(Persona).
+
+puedeAndar(ventas,lucia).
+
+profesional(Persona):- contador(Persona).
+profesional(Persona):- abogado(Persona).
+profesional(Persona):- ingeniero(Persona).
+
+ambicioso(Persona):- contador(Persona), joven(Persona).
+
+conExperiencia(Persona):- trabajoEn(Persona,_).
+
+%Roque
+contador(roque).
+joven(roque).
+trabajoEn(roque,acme).
+habla(roque,frances).
+honesto(roque).
+
+%Ana
+ingeniero(ana).
+habla(ana,ingles).
+habla(ana,frances).
+trabajoEn(ana,omni).
+
+%Lucia
+habla(lucia,ingles).
+habla(lucia,frances).
+trabajoEn(lucia,omni).
+
+%Cecilia
+abogado(cecilia).
+ambicioso(cecilia).
+habla(cecilia,frances).
+
+%Magolla
+abogado(magolla).
+joven(magolla).
+
+%Tests
+test(test_puedeAndar_proyectos_ana):-
+puedeAndar(proyectos,ana).
+
+test(test_puedeAndar_proyectos_magolla):-
+puedeAndar(proyectos,magolla).
+
+test(test_no_puedeAndar_proyectos_roque):-
+not(puedeAndar(proyectos,roque)).
+
+test(test_no_puedeAndar_proyectos_lucia):-
+not(puedeAndar(proyectos,lucia)).
+
+test(test_puedeAndar_logistica_ana):-
+puedeAndar(logistica,ana).
+
+test(test_puedeAndar_logistica_roque):-
+puedeAndar(logistica,roque).
+
+test(test_no_puedeAndar_logistica_lucia):-
+not(puedeAndar(logistica,lucia)).
+
+test(test_no_puedeAndar_logistica_cecilia):-
+not(puedeAndar(logistica,cecilia)).
+
+test(test_fulgencio_proyectosSi_logisticaNo):-
+puedeAndar(proyectos,fulgencio),
+not(puedeAndar(logistica,fulgencio)).
+
+test(test_clotilde_ventasSi_contaduriaNo):-
+puedeAndar(ventas,clotilde),
+not(puedeAndar(contaduria,clotilde)).
+
+
 test(puede_andar_forma_mixta):-
 	aparecePredicado(puedeAndar, mixto).
 
@@ -27,27 +109,3 @@ test(lucia_puede_andar_en_ventas):-
 
 test(ana_puede_andar_en_comercio_exterior):-
 	comercioExterior(ana, si).
-
-test(ana_puede_andar_en_proyectos):-
-	puedeAndar(proyectos, ana).
-
-test(cecilia_no_puede_andar_en_proyectos):-
-	not(puedeAndar(proyectos, cecilia)).
-
-test(lucia_no_puede_andar_en_proyectos):-
-	not(puedeAndar(proyectos, lucia)).
-
-test(ana_puede_andar_en_logistica):-
-	puedeAndar(logistica, ana).
-
-test(roque_puede_andar_en_logistica):-
-	puedeAndar(logistica, roque).
-
-test(cecilia_no_puede_andar_en_logistica):-
-	not(puedeAndar(logistica, cecilia)).
-
-test(juan_puede_andar_en_proyecto_pero_no_en_logistica):-
-	puedeAndar(proyecto, juan), not(puedeAndar(logistica, juan)).
-
-test(maria_puede_andar_en_ventas_pero_no_en_contaduria):-
-	puedeAndar(ventas, maria), not(puedeAndar(contaduria, maria)).
