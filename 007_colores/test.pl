274ed17cd96d0f9bd82f5874a3b5121171f4507c
Rodolfo Caputo <rodcap.93@gmail.com>
2015-03-22 04:12:22 -0300

    Agrego tests ej 7

--- 007_colores/test.pl
+++ 007_colores/test.pl
@@ -1,23 +1,25 @@
-test(test_rta_b):-
-findall(Respuesta,respuesta(b,Respuesta),Respuestas),
-member(nora,Respuestas),
-member(ana,Respuestas),
-member(nuria,Respuestas),
-member(zulema,Respuestas).
+test(test_leAtare_juan):-
+findall(Color,leAtrae(juan, Color),Colores),
+member(rosa, Colores),
+member(celeste, Colores),
+member(lila, Colores),
+member(naranja, Colores).
 
-test(test_rta_c):-
-findall(Respuesta,respuesta(c,Respuesta),Respuestas),
-member(marcos,Respuestas),
-member(roque,Respuestas),
-member(juan,Respuestas).
 
-test(test_rta_d):-
-findall(Respuesta,respuesta(d,Respuesta),Respuestas),
-member(zulema,Respuestas).
+test(test_leAtare_pablo):-
+findall(Color,leAtrae(pablo, Color),Colores),
+member(amarillo, Colores),
+member(azul, Colores),
+member(rojo, Colores).
 
-test(test_rta_e):-
-findall(Respuesta,respuesta(e,Respuesta),Respuestas),
-member(roque,Respuestas),
-member(juan,Respuestas),
+test(test_leAtare_mabel):-
+findall(Color,leAtrae(mabel, Color),Colores),
+member(rosa, Colores),
+member(azul, Colores),
+member(amarillo, Colores).
 
+test(test_leAtare_ana):-
+findall(Color,leAtrae(ana, Color),Colores),
+member(rosa, Colores),
+member(naranja, Colores).
 
