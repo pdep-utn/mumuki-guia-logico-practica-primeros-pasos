c8f82e29115641b78563339f0407947cce0f8087
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-03-29 17:41:25 -0300

    Update test.pl

--- 004_agenciaMatrimonial/test.pl
+++ 004_agenciaMatrimonial/test.pl
@@ -1,20 +1,20 @@
-test(test_esCompatibleCon_maria_juan):-
+test(esCompatibleCon_maria_juan):-
 esCompatibleCon(maria,juan).
 
-test(test_esCompatibleCon_ursula_pedro):-
+test(esCompatibleCon_ursula_pedro):-
 esCompatibleCon(ursula,pedro).
 
-test(test_esCompatibleCon_maria_juan):-
+test(esCompatibleCon_maria_juan):-
 esCompatibleCon(juana,juan).
 
-test(test_no_esCompatibleCon_maria_pedro):-
+test(no_esCompatibleCon_maria_pedro):-
 not(esCompatibleCon(maria,pedro)).
 
-test(test_no_esCompatibleCon_juana_jose):-
+test(no_esCompatibleCon_juana_jose):-
 not(esCompatibleCon(juana,jose)).
 
-test(test_esDeseable_juan):-
+test(esDeseable_juan):-
 esDeseable(juan).
 
-test(test_no_esDeseable_pedro):-
-not(esDeseable(pedro)).
+test(no_esDeseable_jose):-
+not(esDeseable(jose)).
