60d03312aac0df02a92999c914e81f6090d8af2c
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-03-29 15:15:48 -0300

    Update test.pl

--- 007_colores/test.pl
+++ 007_colores/test.pl
@@ -1,37 +1,60 @@
-test(test_leAtare_juan):-
-findall(Color,leAtrae(juan, Color),Colores),
-member(rosa, Colores),
-member(celeste, Colores),
-member(lila, Colores),
-member(naranja, Colores).
+test(a_juan_le_atrae_el_rosa):-
+  atraeA(juan, rosa).
+  
+
+test(a_juan_le_atrae_el_celeste):-
+  atraeA(juan, celeste).
+
+test(a_juan_le_atrae_el_naranja):-
+  atraeA(juan, naranja).
+
+test(a_pablo_le_atrae_el_azul):-
+  atraeA(pablo, azul).
 
 
-test(test_leAtare_pablo):-
-findall(Color,leAtrae(pablo, Color),Colores),
-member(amarillo, Colores),
-member(azul, Colores),
-member(rojo, Colores).
+test(a_pablo_le_atrae_el_rojo):-
+  leAtrae(pablo, rojo).
 
-test(test_leAtare_mabel):-
-findall(Color,leAtrae(mabel, Color),Colores),
-member(rosa, Colores),
-member(azul, Colores),
-member(amarillo, Colores).
+test(a_pablo_NO_le_atrae_el_lila):-
+  \+ atraeA(pablo, lila).
 
-test(test_leAtare_ana):-
-findall(Color,leAtrae(ana, Color),Colores),
-member(rosa, Colores),
-member(naranja, Colores).
+  
+test(a_mabel_le_atrae_el_azul):-
+  atraeA(mabel, azul).
+  
 
-test(test_viernes_puedeIluminarseCon):-
-findall(Color,puedeIluminarseCon(viernes, Color),Colores),
-member(amarillo, Colores),
-member(azul, Colores).
+test(a_mabel_le_atrae_el_rosa):-
+  atraeA(mabel, rosa).
+  
 
-test(test_domingo_puedeIluminarseCon):-
-findall(Color,puedeIluminarseCon(domingo, Color),Colores),
-member(naranja, Colores),
-member(rosa, Colores).
+test(a_mabel_le_atrae_el_amarillo):-
+  atraeA(mabel, amarillo).
 
-test(test_viernes_no_puedeIluminarseCon_rosa):-
-not(puedeIluminarseCon(viernes,rosa)).
+
+test(a_ana_le_atrae_el_naranja):-
+  atraeA(ana, naranja).
+  
+
+test(a_ana_NO_le_atrae_el_amarilo):-
+  \+ atraeA(ana, amarillo).
+  
+test(a_ana_le_atrae_el_rosa):-
+  atraeA(ana, rosa).
+  
+test(viernes_NO_puedeIluminarseCon_naranja):-
+ \+ puedeIluminarseCon(viernes, naranja).
+
+test(viernes_puedeIluminarseCon_azul):-
+ puedeIluminarseCon(viernes, azul).
+
+test(domingo_NO_puede_iluminarse_con_naranja):-
+  \+ puedeIluminarseCon(domingo, lila).
+
+test(domingo_puede_iluminarse_con_naranja):-
+  puedeIluminarseCon(domingo, naranja).
+
+test(domingo_puede_iluminarse_con_rosa):-
+  puedeIluminarseCon(domingo, rosa).
+
+test(viernes_no_puedeIluminarseCon_rosa):-
+  not(puedeIluminarseCon(viernes,rosa)).
