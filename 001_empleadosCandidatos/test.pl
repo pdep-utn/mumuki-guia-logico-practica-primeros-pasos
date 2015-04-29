ce4438bedcb765f48858dbebad2281f96427f6a5
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-04-29 20:15:29 -0300

    Formato y nombres de tests

--- 001_empleadosCandidatos/test.pl
+++ 001_empleadosCandidatos/test.pl
@@ -1,51 +1,51 @@
-test(test_puedeAndar_proyectos_ana):-
+test(puedeAndar_proyectos_ana):-
   puedeAndar(proyectos,ana).
 
-test(test_no_puedeAndar_proyectos_roque):-
+test(no_puedeAndar_proyectos_roque):-
   not(puedeAndar(proyectos,roque)).
 
-test(test_no_puedeAndar_proyectos_lucia):-
- not(puedeAndar(proyectos,lucia)).
+test(no_puedeAndar_proyectos_lucia):-
+  not(puedeAndar(proyectos,lucia)).
 
-test(test_puedeAndar_logistica_ana):-
-puedeAndar(logistica,ana).
+test(puedeAndar_logistica_ana):-
+  puedeAndar(logistica,ana).
 
-test(test_puedeAndar_logistica_roque):-
-puedeAndar(logistica,roque).
+test(puedeAndar_logistica_roque):-
+  puedeAndar(logistica,roque).
 
-test(test_no_puedeAndar_logistica_lucia):-
-not(puedeAndar(logistica,lucia)).
+test(no_puedeAndar_logistica_lucia):-
+  not(puedeAndar(logistica,lucia)).
 
-test(test_no_puedeAndar_logistica_cecilia):-
-not(puedeAndar(logistica,cecilia)).
+test(no_puedeAndar_logistica_cecilia):-
+  not(puedeAndar(logistica,cecilia)).
 
-test(test_fulgencio_proyectosSi_logisticaNo):-
-puedeAndar(proyectos,fulgencio),
-not(puedeAndar(logistica,fulgencio)).
+test(fulgencio_proyectosSi_logisticaNo):-
+  puedeAndar(proyectos,fulgencio),
+  not(puedeAndar(logistica,fulgencio)).
 
 test(clotilde_no_anda_en_contaduria):-
   \+ puedeAndar(contaduria,clotilde).
 
 test(roque_puede_andar_en_comercio_exterior):-
-	puedeAndar(comercioExterior, roque).
+  puedeAndar(comercioExterior, roque).
 
 test(roque_puede_andar_en_contaduria):-
-	puedeAndar(contaduria, roque).
+  puedeAndar(contaduria, roque).
 
 test(roque_puede_andar_en_ventas):-
-	puedeAndar(ventas, roque).
+  puedeAndar(ventas, roque).
 
 test(lucia_no_puede_andar_en_comercio_exterior):-
-	\+ puedeAndar(comercioExterior, lucia).
+  \+ puedeAndar(comercioExterior, lucia).
 
 test(lucia_puede_andar_en_ventas):-
-	puedeAndar(ventas, lucia).
+  puedeAndar(ventas, lucia).
 
 test(ana_puede_andar_en_comercio_exterior):-
-	puedeAndar(comercioExterior, ana).
+  puedeAndar(comercioExterior, ana).
 
 test(fulgencio_puede_andar_en_proyectos):-
-	puedeAndar(proyectos, fulgencio).
+  puedeAndar(proyectos, fulgencio).
 
 test(clotilde_puede_andar_en_ventas):-
-	puedeAndar(ventas, clotilde).
+  puedeAndar(ventas, clotilde).
