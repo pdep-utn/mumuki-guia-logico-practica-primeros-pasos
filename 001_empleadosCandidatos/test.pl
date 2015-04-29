d989df8f8d4a2b77b41f282f15dfc6d3accd8444
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-04-29 20:16:08 -0300

    Marcando a los predicados como nondet

--- 001_empleadosCandidatos/test.pl
+++ 001_empleadosCandidatos/test.pl
@@ -1,51 +1,51 @@
-test(puedeAndar_proyectos_ana):-
+test(puedeAndar_proyectos_ana, [nondet]):-
   puedeAndar(proyectos,ana).
 
-test(no_puedeAndar_proyectos_roque):-
+test(no_puedeAndar_proyectos_roque, [nondet]):-
   not(puedeAndar(proyectos,roque)).
 
-test(no_puedeAndar_proyectos_lucia):-
+test(no_puedeAndar_proyectos_lucia, [nondet]):-
   not(puedeAndar(proyectos,lucia)).
 
-test(puedeAndar_logistica_ana):-
+test(puedeAndar_logistica_ana, [nondet]):-
   puedeAndar(logistica,ana).
 
-test(puedeAndar_logistica_roque):-
+test(puedeAndar_logistica_roque, [nondet]):-
   puedeAndar(logistica,roque).
 
-test(no_puedeAndar_logistica_lucia):-
+test(no_puedeAndar_logistica_lucia, [nondet]):-
   not(puedeAndar(logistica,lucia)).
 
-test(no_puedeAndar_logistica_cecilia):-
+test(no_puedeAndar_logistica_cecilia, [nondet]):-
   not(puedeAndar(logistica,cecilia)).
 
-test(fulgencio_proyectosSi_logisticaNo):-
+test(fulgencio_proyectosSi_logisticaNo, [nondet]):-
   puedeAndar(proyectos,fulgencio),
   not(puedeAndar(logistica,fulgencio)).
 
-test(clotilde_no_anda_en_contaduria):-
+test(clotilde_no_anda_en_contaduria, [nondet]):-
   \+ puedeAndar(contaduria,clotilde).
 
-test(roque_puede_andar_en_comercio_exterior):-
+test(roque_puede_andar_en_comercio_exterior, [nondet]):-
   puedeAndar(comercioExterior, roque).
 
-test(roque_puede_andar_en_contaduria):-
+test(roque_puede_andar_en_contaduria, [nondet]):-
   puedeAndar(contaduria, roque).
 
-test(roque_puede_andar_en_ventas):-
+test(roque_puede_andar_en_ventas, [nondet]):-
   puedeAndar(ventas, roque).
 
-test(lucia_no_puede_andar_en_comercio_exterior):-
+test(lucia_no_puede_andar_en_comercio_exterior, [nondet]):-
   \+ puedeAndar(comercioExterior, lucia).
 
-test(lucia_puede_andar_en_ventas):-
+test(lucia_puede_andar_en_ventas, [nondet]):-
   puedeAndar(ventas, lucia).
 
-test(ana_puede_andar_en_comercio_exterior):-
+test(ana_puede_andar_en_comercio_exterior, [nondet]):-
   puedeAndar(comercioExterior, ana).
 
-test(fulgencio_puede_andar_en_proyectos):-
+test(fulgencio_puede_andar_en_proyectos, [nondet]):-
   puedeAndar(proyectos, fulgencio).
 
-test(clotilde_puede_andar_en_ventas):-
+test(clotilde_puede_andar_en_ventas, [nondet]):-
   puedeAndar(ventas, clotilde).
