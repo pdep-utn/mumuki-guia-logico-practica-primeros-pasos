23c3a4fe35ebb97d3296f01a954387a0e241bbc9
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-05-03 13:21:01 -0300

    Typo

--- 040_daOrdenes/test.pl
+++ 040_daOrdenes/test.pl
@@ -1,15 +1,15 @@
 test(no_se_puede_dar_ordenes_a_alguien_de_diferente_departamento) :-
-  \+ puedeDarOrdenes(ingrid, hernan).
+  \+ daOrdenes(ingrid, hernan).
 
 test(no_se_puede_dar_ordenes_a_alguien_de_mismo_puesto) :-
-  \+ puedeDarOrdenes(anastasia, camilo).
+  \+ daOrdenes(anastasia, camilo).
 
 test(no_se_puede_dar_ordenes_a_alguien_de_mismo_puesto_en_otro_departamento) :-
-  \+ puedeDarOrdenes(ingrid, maquiavelo).
+  \+ daOrdenes(ingrid, maquiavelo).
 
 test(nadie_puede_darle_ordenes_si_no_tiene_gerente) :-
-  \+ puedeDarOrdenes(_, tomas).
+  \+ daOrdenes(_, tomas).
 
 test(un_gerente_puede_darle_ordenes_a_un_cadete_de_su_departamento) :-
-  puedeDarOrdenes(ingrid, anastasia),
-  puedeDarOrdenes(ingrid, camilo).
+  daOrdenes(ingrid, anastasia),
+  daOrdenes(ingrid, camilo).
