877ba23cdeec3388541e822100867848bd19590d
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-05-03 15:04:59 -0300

    Marcando algunos tests como nondet

--- 005_gustaDe/test.pl
+++ 005_gustaDe/test.pl
@@ -4,19 +4,19 @@ test(a_luis_le_gusta_nora, [nondet]) :-
 test(a_juan_no_le_gusta_roque) :-
   \+ gustaDe(juan, roque).
 
-test(a_marcos_le_gusta_zulema) :-
+test(a_marcos_le_gusta_zulema, [nondet]) :-
   gustaDe(marcos, zulema).
 
-test(a_juan_le_gusta_nuria) :-
+test(a_juan_le_gusta_nuria, [nondet]) :-
   gustaDe(juan, nuria).
 
-test(roque_gusta_de_zulema) :-
+test(roque_gusta_de_zulema, [nondet]) :-
   gustaDe(roque, zulema).
 
-test(juan_gusta_de_zulema) :-
+test(juan_gusta_de_zulema, [nondet]) :-
   gustaDe(juan, zulema).
 
-test(juan_gusta_de_ana) :-
+test(juan_gusta_de_ana, [nondet]) :-
   gustaDe(juan, ana).
 
 test(marcos_no_gusta_de_ana) :-
@@ -34,12 +34,12 @@ test(a_nora_no_le_gusta_nadie) :-
 test(a_ana_no_le_gusta_nadie) :-
   \+ gustaDe(ana, _).
 
-test(a_zulema_le_gusta_ana) :-
+test(a_zulema_le_gusta_ana, [nondet]) :-
   gustaDe(zulema, ana).
 
 test(a_zulema_no_le_gusta_zulema) :-
   \+ gustaDe(zulema, zulema).
 
-test(gustaDe_es_inversible) :-
+test(gustaDe_es_inversible, [nondet]) :-
   findall((X, Y), gustaDe(X, Y), Atracciones),
   member((roque, zulema), Atracciones).
